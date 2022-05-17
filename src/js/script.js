8e47342905346b0d94f76f79d54395368a078a1f
Yevhenii <zhenia1717@gmail.com>
2022-05-17 23:15:56 +0300

    add slick to page

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,17 @@
-for (let i = 1; i < 8; i++) {
-    console.log(i);
-}
-
+$(document).ready(function () {
+    $('.carusel__inner').slick({
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/icon/arrow-left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/icon/arrow-right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+});
