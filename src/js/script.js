e5c98181d31d64bef15731eeda8022ce72fd838f
Yevhenii <zhenia1717@gmail.com>
2022-05-18 23:05:23 +0300

    slick.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('.carusel__inner').slick({
+    $('.carousel__inner').slick({
         speed: 1200,
         // adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="img/icon/arrow-left.png"></button>',
