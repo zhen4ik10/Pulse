cfb3a2a8b15d721746e661ed9e4b8391e8dbc5ea
Yevhenii <zhenia1717@gmail.com>
2022-05-21 00:30:53 +0300

    add tabs to page

--- src/js/script.js
+++ src/js/script.js
@@ -14,4 +14,23 @@ $(document).ready(function () {
             }
         ]
     });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab--active)', function () {
+        $(this)
+            .addClass('catalog__tab--active').siblings().removeClass('catalog__tab--active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content--active').eq($(this).index()).addClass('catalog__content--active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content--active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list--active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
 });
